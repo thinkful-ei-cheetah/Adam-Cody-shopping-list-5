2808e4e2bb4161aaf01d7f90bcf13a4d8b6b581b
Cody Gillette <cgillette1312@gmail.com>
2019-04-03 09:45:42 -0700

    stuff

--- scripts/api.js
+++ scripts/api.js
@@ -1,16 +1,33 @@
 'use strict';
+/* global shoppingList, store, Item, api */
 const api = (function (){
-    const BASE_URL = 'https://thinkful-list-api.herokuapp.com/cody/items/';
+  const BASE_URL = 'https://thinkful-list-api.herokuapp.com/cody/items/';
 
-    function getItems(){
-        fetch(BASE_URL)
-            .then(response => response.json())
-            .then(data => data);
-        //return Promise.resolve('A successful response!');
-    }
-    
+  function getItems(){
+    return  fetch(BASE_URL + '', {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  }
+  
 
-    return {
-        getItems
+  function createItem(name){
+    let newItem ={
+      name,
     };
+    return JSON.stringify(newItem);
+  }
+
+  //     return  fetch(BASE_URL + 'POST', {
+  //       headers: {
+  //         'Content-Type': 'application/json'
+  //       }
+  //         .then(stringObj)
+  //     });
+  //   }
+
+  return {
+    getItems,createItem
+  };
 }());
--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,29 @@
 'use strict';
-/* global shoppingList, store, Item, api */
+/* global $,shoppingList, store, Item, api */
 
 $(document).ready(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
+  // api.getItems()
+  //   .then(response => response.json())
+  //   .then(data => {
+  //     console.log(data);
+  //   });
+  api.createItem('pears')
+    .then(res => res.json())
+    .then((newItem) => {
+      return api.getItems();
+    })
+    .then(res => res.json())
+    .then((items) => {
+      console.log(items);
+    });
 });
 
 store.items.push(Item.create('apples'));
 
 
-api.getItems()
-  .then(res => console.log(res));
+  
+
 
 console.log(api.BASE_URL);
