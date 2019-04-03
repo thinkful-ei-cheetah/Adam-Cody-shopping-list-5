7ab43616332bdb42111a9e27f717767297f3b968
Cody Gillette <cgillette1312@gmail.com>
2019-04-03 15:47:50 -0700

    trying to find problem

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,12 @@
 
 $(document).ready(function() {
   shoppingList.bindEventListeners();
-  shoppingList.render();
+  // api.getItems()
+  //   .then((items) => {
+  //     items.forEach((item) => store.addItem(item));
+  //     shoppingList.render();
+  //   })
+  //   .catch(err => console.log(err.message));
  
 });
 
